28c150e4dffba16099727997983056ce22660f58
funpi89 <a0922810725@gmail.com>
2021-11-27 23:53:59 +0800

    feat(crm): add crm UserProfile, ContactForm model

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from crm.models import UserProfile, ContactForm
 # Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(ContactForm)
--- crm/models.py
+++ crm/models.py
@@ -8,3 +8,20 @@ class UserProfile(models.Model):
     user = models.ForeignKey(User, verbose_name='使用者', on_delete=models.CASCADE)
     phone = models.CharField('電話', max_length=15, blank=True, null=True)
     address = models.CharField('地址', max_length=127, blank=True, null=True)
+
+    class Meta:
+        verbose_name = '使用者檔案'
+        verbose_name_plural = '使用者檔案'
+
+
+class ContactForm(models.Model):
+    user = models.ForeignKey(User, verbose_name='使用者', on_delete=models.CASCADE)
+    phone = models.CharField('電話', max_length=15, blank=True, null=True)
+    email = models.EmailField()
+    created_at = models.DateTimeField('建立日期', auto_now_add=True)
+    contacted = models.BooleanField('已聯絡', default=False)
+
+    class Meta:
+        verbose_name = '聯絡表單'
+        verbose_name_plural = '聯絡表單'
+
